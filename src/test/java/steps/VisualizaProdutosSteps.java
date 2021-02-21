25bf3e86385611817c5c4d714b9ad134e0325017
Dernival Liandro <liandro.sys@gmail.com>
2021-02-21 13:40:36 -0300

    Implementacao do Cucumber nos testes

---
+++ src/test/java/steps/VisualizaProdutosSteps.java
@@ -0,0 +1,55 @@
+package steps;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+import pages.HomePage;
+
+public class VisualizaProdutosSteps {
+	
+	private static WebDriver driver;
+	private HomePage homePage = new HomePage(driver);
+	
+	@Before
+	public static void inicializar() {
+		System.setProperty("webdriver.chrome.driver", "/home/dernival_liandro/.webdrivers/chromedriver/88/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	@After
+	public static void finalizar() {
+		driver.quit();
+	}
+	
+	@Dado("que estou na pagina inicial")
+	public void que_estou_na_pagina_inicial() {
+		homePage.carregarPaginaIncial();
+		assertThat(homePage.obterTituloPagina(), is("Loja de Teste"));
+	}
+
+	@Quando("nao estou logado")
+	public void nao_estou_logado() {
+		assertThat(homePage.naoEstaLogado(), is(true));
+	}
+
+	@Entao("visualizo {int} produtos disponiveis")
+	public void visualizo_produtos_disponiveis(Integer int1) {
+		assertThat(homePage.contarProdutos(), is(int1));
+	}
+
+	@Entao("carrinho esta zerado")
+	public void carrinho_esta_zerado() {
+		assertThat(homePage.obterQuantidadeProdutosNoCarrinho(), is(0));
+	}
+
+}
